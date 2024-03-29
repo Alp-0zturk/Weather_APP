1f2644ee72f94567a2bc2c8856f56867120bb833
Alp <139057304+Alp-0zturk@users.noreply.github.com>
2024-03-29 13:43:17 +0100

    Update weather_analysis.py

--- weather_analysis.py
+++ weather_analysis.py
@@ -1,13 +1,10 @@
-#from retry_requests import retry
-#from geopy.geocoders import Nominatim
 import requests
 import datetime
 
-#geolocator = Nominatim(user_agent="MyApp")
 apikey:str = 'f6e2abf6d1b2b030187c12b6254c374c'
 
 #import weather_UI as w_UI
-def get_weather_data(city):    #openweatherAPI  #PART2
+def get_weather_data(city):    #openweatherAPI
     weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={apikey}")
     if weather_data.json()['cod'] == '404':
         print("there is no such city bruh")
@@ -16,21 +13,10 @@ def get_weather_data(city):    #openweatherAPI  #PART2
         temp:int = round(weather_data.json()['main']['temp'])
         #humidity = round(weather_data.json()['main']['json'])
         return weather,temp
-'''
-def cityToCOORD(city): #part 1
-    location = geolocator.geocode(city)
-    lat:float = location.latitude
-    lon:float = location.longitude
-    address:str = location
-    return lat, lon, address '''
 
 def main():
     print("please enter a city name to get live weather data")
     city:str = input()
-    '''
-    lat:float
-    lon:float  
-    lat, lon, address = cityToCOORD(city)'''
     
     address:str
     weather:str
