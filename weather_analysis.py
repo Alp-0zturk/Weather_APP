a2c6c7abe6dfe5db89885663b8ec755657764f68
Alp <139057304+Alp-0zturk@users.noreply.github.com>
2024-03-29 13:35:42 +0100

    update

--- weather_analysis.py
+++ weather_analysis.py
@@ -1,89 +1,45 @@
-import requests_cache
-import pandas as pd
-import time
-from retry_requests import retry
-from openmeteo_requests import Client
-from requests.adapters import HTTPAdapter
-from geopy.geocoders import Nominatim
-from requests.packages.urllib3.util.retry import Retry
-geolocator = Nominatim(user_agent="MyApp")
+#from retry_requests import retry
+#from geopy.geocoders import Nominatim
+import requests
+import datetime
+
+#geolocator = Nominatim(user_agent="MyApp")
+apikey:str = 'f6e2abf6d1b2b030187c12b6254c374c'
 
 #import weather_UI as w_UI
-def get_weather_data(lat, lon):    #open-meteo API  # PART2
-    url = "https://api.open-meteo.com/v1/forecast"
-    params = {
-	"latitude": lat,
-	"longitude": lon,
-    }
-    try:
-        responses = openmeteo.weather_api(url, params=params)
-        response = responses[0]
-    except Exception as e:
-        print(f"Error fetching weather data: {e}")
-        return None
-    
-    retry_strategy = Retry(
-    total=5,
-    backoff_factor=0.2,
-    status_forcelist=[429, 500, 502, 503, 504],
-    method_whitelist=["GET"],
-    )
-    adapter = HTTPAdapter(max_retries=retry_strategy)
-    retry_session = requests_cache.CachedSession('.cache', expire_after=3600)
-    retry_session.mount("https://", adapter)
-    openmeteo = Client(session=retry_session)
-
-def get_hourly_temperature(lat, lon):
-    url = "https://api.open-meteo.com/v1/forecast"
-    params = {
-        "latitude": lat,
-        "longitude": lon,
-    }
-    for i in range(5):  # Retry 5 times
-        try:
-            cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
-            retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
-            openmeteo = Client(session=retry_session)
-            responses = openmeteo.weather_api(url, params=params)
-
-        # Process first location. Add a for-loop for multiple locations or weather models
-            response = responses[0]
-
-        # Check if the response has the attribute 'hourly_data'
-            if hasattr(response, 'hourly_data'):
-                hourly_temperatures = response.hourly_data('temperature_2m')
-                # Print hourly temperature data
-                for hour, temp in hourly_temperatures.items():
-                    print(f"Hour: {hour}, Temperature: {temp}°C")
-                    break  # If the request is successful, break the loop
-            else:
-                print("The response object does not have the 'hourly_data' attribute.")
-        except:
-            time.sleep(5)
-
-def cityToCOORD(city):
+def get_weather_data(city):    #openweatherAPI  #PART2
+    weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={apikey}")
+    if weather_data.json()['cod'] == '404':
+        print("there is no such city bruh")
+    else:
+        weather:str = weather_data.json()['weather'][0]['main']
+        temp:int = round(weather_data.json()['main']['temp'])
+        #humidity = round(weather_data.json()['main']['json'])
+        return weather,temp
+'''
+def cityToCOORD(city): #part 1
     location = geolocator.geocode(city)
     lat:float = location.latitude
     lon:float = location.longitude
     address:str = location
-    return lat, lon, address
+    return lat, lon, address '''
 
 def main():
     print("please enter a city name to get live weather data")
     city:str = input()
+    '''
     lat:float
-    lon:float
+    lon:float  
+    lat, lon, address = cityToCOORD(city)'''
+    
     address:str
-    lat, lon, address = cityToCOORD(city)
-    lat = round(lat, 2)
-    lon = round(lon, 2)
-    print("address:\n", address, "\n", "latitude:\n", lat, "°N\n", "longitude:\n", lon, "°E\n")
-
-    weather_data = get_weather_data(lat, lon)
-    temper = get_hourly_temperature(lat, lon)
-    print("The address:", "\n", address)
-    print(weather_data)
-    print(temper)
-
+    weather:str
+    temp:int
+    
+    address, weather, temp = get_weather_data(city)
+    print("address: ", address)
+    print("Weather status: ", weather)
+    print("temperature: ",temp)
+    
 if __name__ == "__main__":
     main()
